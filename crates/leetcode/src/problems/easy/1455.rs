a69c7a03c94315501c7fb3e26d75bded7605f310
voidptrr <bruno.tommaso@protonmail.com>
2026-04-15 16:50:20 +0100

    feat: problem 1455

---
+++ crates/leetcode/src/problems/easy/1455.rs
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence
+
+pub struct Solution;
+
+impl Solution {
+    pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
+        for (index, word) in sentence.split(' ').enumerate() {
+            if word.starts_with(&search_word) {
+                return (index + 1) as i32
+            }
+        };
+
+        -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn example_one() {
+        assert_eq!(
+            Solution::is_prefix_of_word(
+                "i love eating burger".to_owned(),
+                "burg".to_owned()
+            ),
+            4
+        );
+    }
+
+    #[test]
+    fn example_two() {
+        assert_eq!(
+            Solution::is_prefix_of_word(
+                "this problem is an easy problem".to_owned(),
+                "pro".to_owned()
+            ),
+            2
+        );
+    }
+
+    #[test]
+    fn example_three() {
+        assert_eq!(
+            Solution::is_prefix_of_word(
+                "i am tired".to_owned(),
+                "you".to_owned()
+            ),
+            -1
+        );
+    }
+}
--- crates/leetcode/src/problems/easy/mod.rs
+++ crates/leetcode/src/problems/easy/mod.rs
@@ -4,6 +4,9 @@ pub mod problem_1108;
 #[path = "1394.rs"]
 pub mod problem_1394;
 
+#[path = "1455.rs"]
+pub mod problem_1455;
+
 #[path = "1464.rs"]
 pub mod problem_1464;
 
