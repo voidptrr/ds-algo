4b296807e9fa12e01defabc8ee0c1977461ae8d2
voidptrr <bruno.tommaso@protonmail.com>
2026-04-30 17:01:14 +0100

    feat: use counting sort for improve time efficiency

--- crates/leetcode/src/problems/medium/smallest_value_of_the_rearranged_number.rs
+++ crates/leetcode/src/problems/medium/smallest_value_of_the_rearranged_number.rs
@@ -3,7 +3,34 @@
 pub struct Solution;
 
 impl Solution {
+    fn counting_sort(input: &[i64], result: &mut [i64]) {
+        if input.is_empty() {
+            return;
+        }
+
+        let max = *input.iter().max().unwrap() as usize;
+        let mut freq = vec![0usize; max + 1];
+
+        for &digit in input {
+            freq[digit as usize] += 1;
+        }
+
+        for i in 1..freq.len() {
+            freq[i] += freq[i - 1];
+        }
+
+        for &digit in input.iter().rev() {
+            let d = digit as usize;
+            freq[d] -= 1;
+            result[freq[d]] = digit;
+        }
+    }
+
     pub fn smallest_number(num: i64) -> i64 {
+        if num == 0 {
+            return 0;
+        }
+
         let mut digits = Vec::new();
 
         let mut num_mut = num;
@@ -18,12 +45,15 @@ impl Solution {
             num_mut /= 10;
         }
 
-        digits.sort_by(|a, b| if is_negative { b.cmp(a) } else { a.cmp(b) });
+        let mut result = vec![0; digits.len()];
+        Self::counting_sort(&digits, &mut result);
 
-        if !is_negative && let Some(idx) = digits.iter().position(|&d| d != 0) {
-            digits.swap(0, idx);
+        if is_negative {
+            result.reverse();
+        } else if let Some(idx) = result.iter().position(|&d| d != 0) {
+            result.swap(0, idx);
         }
-        let n: i64 = digits.into_iter().fold(0i64, |acc, d| acc * 10 + d);
+        let n: i64 = result.into_iter().fold(0i64, |acc, d| acc * 10 + d);
         if is_negative { -n } else { n }
     }
 }
